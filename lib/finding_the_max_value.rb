0b05e29e6740cd27cf3b0285f01eeb082efa3af8
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-08 16:38:37 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,10 @@
 def find_max_value(array)
-  array.sort!
-  return array.last 
+  counter = 0 
+  max_value = -1
+  when array[counter] do
+    if max_value < array[counter]
+      max_value = array[counter]
+    end
+    return max_value
+  end
 end
